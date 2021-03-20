a24ee6ffac5df0e5f40481927410f2e31f3708a9
5tarlight <developerstarlight@gmail.com>
2021-03-20 12:50:47 +0900

    minus item

--- CGamza/Inventory/Inventory.cs
+++ CGamza/Inventory/Inventory.cs
@@ -25,18 +25,18 @@ namespace CGamza.Inventory
       }
     }
 
-    public List<InventoryItem> items;
+    public List<InventoryItem> Items;
     public int Limit { get; }
 
     public Inventory()
     {
       Limit = 100;
-      items = new List<InventoryItem>();
+      Items = new List<InventoryItem>();
     }
 
     public bool Has(CItem item)
     {
-      var has = from i in items
+      var has = from i in Items
         where i.Item.Name.Equals(item.Name)
         select i;
 
@@ -47,16 +47,46 @@ namespace CGamza.Inventory
     {
       if (Has(item))
       {
-        var index = items.FindIndex(pi => pi.Item.Equals(item));
-        var current = new InventoryItem(items[index]);
+        var index = Items.FindIndex(pi => pi.Item.Equals(item));
+        var current = new InventoryItem(Items[index]);
         current.Count += count;
 
-        items[index] = current;
+        Items[index] = current;
       }
       else
       {
         var invItem = new InventoryItem(item, count);
-        items.Add(invItem);
+        Items.Add(invItem);
+      }
+    }
+
+    public int GetCount(CItem item)
+    {
+      if (!Has(item)) return 0;
+
+      var index = Items.FindIndex(i => i.Item.Name == item.Name);
+
+      return Items[index].Count;
+    }
+    
+    public bool MinusItem(CItem item, int count)
+    {
+      if (!Has(item)) return false;
+
+      var limit = GetCount(item);
+      if (count > limit) return false;
+      
+      AddItem(item, -count);
+      TrimInv();
+      return true;
+    }
+
+    private void TrimInv()
+    {
+      foreach (var v in Items)
+      {
+        if (v.Count == 0)
+          Items.Remove(v);
       }
     }
   }
