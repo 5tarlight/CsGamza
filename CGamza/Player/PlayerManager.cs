80298da050ce65abb7ad2aae0c09cd7a1b2b35be
5tarlight <developerstarlight@gmail.com>
2021-03-24 07:00:32 +0900

    remove player's hp, ex stats it's because the toward of this game is now finally clear

--- CGamza/Player/PlayerManager.cs
+++ CGamza/Player/PlayerManager.cs
@@ -146,11 +146,6 @@ namespace CGamza.Player
     {
       Util.WriteColor($"이름 : {CurrentPlayer.Name}");
       Util.WriteColor(CurrentPlayer.Profile);
-      Console.WriteLine();
-      Util.WriteColor($"체력 : {CurrentPlayer.Health} / {CurrentPlayer.MaxHealth}");
-      Util.WriteColor($"레벨 : {CurrentPlayer.Level}");
-      Util.WriteColor($"경험치 : {CurrentPlayer.Exp}");
-      Util.WriteColor($"다음 레벨까지 {CurrentPlayer.GetNeedExpForNextLvl()} 남음");
     }
   }
 }
