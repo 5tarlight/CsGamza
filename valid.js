98bdaf92ec4721e7efdff595fe6aa64cbfd46814
5tarlight <developerstarlight@gmail.com>
2021-03-24 08:00:27 +0900

    validation check

--- valid.js
+++ valid.js
@@ -9,24 +9,24 @@ fs.readdir('./data', (err, files) => {
         valid: false,
       })
   })
-})
 
-users.forEach(({ u, v }) => {
-  fs.readdir(`./data/${u}`, (err, files) => {
-    if (
-      files.includes('inventory.idata') &&
-      files.includes('profile.pdata')
-    ) {
-      const index = users.findIndex(us => us.name == u)
-      users[index].valid = true
-    }
+  users.forEach((u) => {
+    fs.readdir(`./data/${u.name}`, (err, files) => {
+      if (
+        files.includes('inventory.idata') &&
+        files.includes('profile.pdata')
+      ) {
+        const index = users.findIndex(us => us.name == u.name)
+        users[index].valid = true
+      }
+
+      console.log(`${users.length} users`)
+      users.forEach(({ name, valid }) => {
+        if (valid)
+          console.log(`${name} - valid`)
+        else
+          console.log(`${valid} - invalid`)
+      })
+    })
   })
-})
-
-console.log(`${users.length} users`)
-users.forEach(({ u, v }) => {
-  if (v)
-    console.log(`${u} - valid`)
-  else
-    console.log(`${u} - invalid`)
 })
