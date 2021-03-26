0625a10cca3842c2524c7683976bf75cc44d38d2
5tarlight <developerstarlight@gmail.com>
2021-03-26 10:44:14 +0900

    select starting pet

--- CGamza/Util/GameManager.cs
+++ CGamza/Util/GameManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using CGamza.Entity.Pet;
 using CGamza.Inventory;
-using CGamza.Item;
 using CGamza.Player;
 using CGamza.Terrain.Town;
 
@@ -56,15 +55,17 @@ namespace CGamza.Util
 
     public static void MainMenu()
     {
-      var q = new List<SelectableQuestion>();
-      q.Add(new SelectableQuestion("캐릭터 확인하기"));
-      q.Add(new SelectableQuestion("캐릭터 바꾸기"));
-      q.Add(new SelectableQuestion("인벤토리 확인하기"));
-      q.Add(new SelectableQuestion("펫 확인하기"));
-      q.Add(new SelectableQuestion("펫 추가하기"));
-      q.Add(new SelectableQuestion("태초마을로"));
-      q.Add(new SelectableQuestion("진달래마을로"));
-      q.Add(new SelectableQuestion("종료"));
+      var q = new List<SelectableQuestion>()
+      {
+        new SelectableQuestion("캐릭터 확인하기"),
+        new SelectableQuestion("캐릭터 바꾸기"),
+        new SelectableQuestion("인벤토리 확인하기"),
+        new SelectableQuestion("펫 확인하기"),
+        new SelectableQuestion("펫 추가하기"),
+        new SelectableQuestion("태초마을로"),
+        new SelectableQuestion("진달래마을로"),
+        new SelectableQuestion("종료")
+      };
       
       var answer = Util.AskSelectableQuestion("무엇을 하시겠습니까", q);
 
@@ -85,7 +86,7 @@ namespace CGamza.Util
           Util.Pause();
           break;
         case 4:
-          PlayerManager.CurrentPlayer.AddPet(new FireSoul());
+          SelectStartPet();
           break;
         case 5:
           PlayerManager.CurrentPlayer.Location.Move(Towns.beginningVillage);
@@ -95,6 +96,33 @@ namespace CGamza.Util
           break;
         case 7:
           Environment.Exit(0);
+          break;
+      }
+    }
+
+    public static void SelectStartPet()
+    {
+      Console.Clear();
+      
+      var q = new List<SelectableQuestion>()
+      {
+        new SelectableQuestion("불의 정령"),
+        new SelectableQuestion("물의 정령"),
+        new SelectableQuestion("풀의 정령")
+      };
+
+      var user = Util.AskSelectableQuestion("펫을 선택하세요.", q);
+
+      switch (user)
+      {
+        case 0:
+          PlayerManager.CurrentPlayer.AddPet(new FireSoul());
+          break;
+        case 1:
+          PlayerManager.CurrentPlayer.AddPet(new WaterSoul());
+          break;
+        case 2:
+          PlayerManager.CurrentPlayer.AddPet(new GrassSoul());
           break;
       }
     }
