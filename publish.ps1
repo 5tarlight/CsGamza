4caae8068df1187dcc574bbe17b024c8b2e86523
5tarlight <developerstarlight@gmail.com>
2021-04-07 15:59:18 +0900

    publisher

---
+++ publish.ps1
@@ -0,0 +1,3 @@
+dotnet publish -c Release -r win10-x64 --self-contained true
+dotnet publish -c Release -r linux-x64 --self-contained true
+dotnet publish -c Release -r osx-x64 --self-contained true
