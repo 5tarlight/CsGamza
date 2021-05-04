d3e57c695fa7a747548cbda04877bd32c177c6c7
5tarlight <developerstarlight@gmail.com>
2021-05-04 09:54:13 +0900

    sh

---
+++ publish.sh
@@ -0,0 +1,4 @@
+dotnet publish -c Release -r win10-x64 --self-contained true
+dotnet publish -c Release -r win7-x64 --self-contained true
+dotnet publish -c Release -r linux-x64 --self-contained true
+dotnet publish -c Release -r osx-x64 --self-contained true
